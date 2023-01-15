0cfc2ff9a744a898be9dd27dd8698816ab6e4a5c
Gintaras <gintaras.gaucys1@gmail.com>
2023-01-15 15:53:54 +0200

    removing file

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-let characters = await camp.characters.get();
-        // for (let i = 0; i < Object.keys(characters["data"]); i++) {
-        //     if (characters["data"][i]["name"] == "Lazarus Flameclaw") {
-        //         console.log(characters["data"][i])
-        //     } else {
-
-        //     }
-        // }
